179ba28fb947352e76b483fa16c4714938ad93b8
bismalif <bisma.alif@ui.ac.id>
2023-06-09 14:14:28 +0700

    update frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 const Navbar = ({ username, email, balance }) => {
   const [activeItem, setActiveItem] = useState('');
+  const location = useLocation();
 
-  const handleItemClick = (item) => {
-    setActiveItem(item);
-
-    if (item === 'Home') {
-      window.location.href = '/Home';
-    } else if (item === 'Categories') {
-      window.location.href = '/Categories';
-    } else if (item === 'Check-out') {
-      window.location.href = '/Check-out';
-    } else if (item === 'History') {
-      window.location.href = '/History';
-    }
-  };
+  useEffect(() => {
+    const { pathname } = location;
+    setActiveItem(pathname);
+  }, [location]);
 
   const handleLogoClick = () => {
     window.location.href = '/Home';
   };
 
   const handleProfileClick = () => {
-    window.location.href = '/Profile';
+    window.location.href = '/Account';
+  };
+
+  const handleCategoryClick = () => {
+    window.location.href = '/Category';
+  };
+
+  const handleCheckOutClick = () => {
+    window.location.href = '/Checkout';
+  };
+
+  const handleHistoryClick = () => {
+    window.location.href = '/History';
   };
 
   return (
@@ -33,37 +38,17 @@ const Navbar = ({ username, email, balance }) => {
             <span className="font-bold text-5xl mr-4">JUALIN</span>
           </div>
           <ul className="flex space-x-8 ml-8">
-            <li
-              className={`text-white font-bold text-lg ${activeItem === 'Home' ? 'underline' : ''} ${
-                window.location.pathname === '/Home' ? 'text-black' : ''
-              }`}
-              onClick={() => handleItemClick('Home')}
-            >
-              Home
+            <li className={`text-white font-bold text-lg ${activeItem === '/Home' ? 'underline' : ''}`} onClick={handleLogoClick}>
+              <Link to="/Home">Home</Link>
             </li>
-            <li
-              className={`text-white font-bold text-lg ${activeItem === 'Categories' ? 'underline' : ''} ${
-                window.location.pathname === '/Categories' ? 'text-black' : ''
-              }`}
-              onClick={() => handleItemClick('Categories')}
-            >
-              Categories
+            <li className={`text-white font-bold text-lg ${activeItem === '/Categories' ? 'underline' : ''}`} onClick={handleCategoryClick}>
+              <Link to="/Categories">Category</Link>
             </li>
-            <li
-              className={`text-white font-bold text-lg ${activeItem === 'Check-out' ? 'underline' : ''} ${
-                window.location.pathname === '/Check-out' ? 'text-black' : ''
-              }`}
-              onClick={() => handleItemClick('Check-out')}
-            >
-              Check-out
+            <li className={`text-white font-bold text-lg ${activeItem === '/Check-out' ? 'underline' : ''}`} onClick={handleCheckOutClick}>
+              <Link to="/Check-out">Check-out</Link>
             </li>
-            <li
-              className={`text-white font-bold text-lg ${activeItem === 'History' ? 'underline' : ''} ${
-                window.location.pathname === '/History' ? 'text-black' : ''
-              }`}
-              onClick={() => handleItemClick('History')}
-            >
-              History
+            <li className={`text-white font-bold text-lg ${activeItem === '/History' ? 'underline' : ''}`} onClick={handleHistoryClick}>
+              <Link to="/History">History</Link>
             </li>
           </ul>
         </div>
@@ -78,11 +63,13 @@ const Navbar = ({ username, email, balance }) => {
         </div>
         <div className="flex items-center space-x-4">
           <div className="flex items-center" onClick={handleProfileClick}>
-            <img
-              className="w-12 h-12 rounded-full"
-              src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg"
-              alt="Profile"
-            />
+            <Link to="/Account">
+              <img
+                className="w-12 h-12 rounded-full"
+                src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg"
+                alt="Profile"
+              />
+            </Link>
           </div>
           <div className="text-white">
             <span className="font-bold text-lg" style={{ fontFamily: 'Jost Bold' }}>
@@ -96,11 +83,10 @@ const Navbar = ({ username, email, balance }) => {
             <span className="text-gray-300 ml-2"></span>
             <br />
             <span className="font-bold text-lg" style={{ fontFamily: 'Jost Bold' }}>
-              Balance: {balance.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}
+              Rp{balance}
             </span>
             <span className="text-gray-300 ml-2"></span>
           </div>
-
         </div>
       </div>
     </nav>
